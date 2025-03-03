3bee109e98ba549f607851c65490cb0871363ff8
Shubham Bansal <20eo1057@mitsgwl.ac.in>
2025-03-04 00:58:44 +0530

    setup middleware for json,url,static files

--- src/index.js
+++ src/index.js
@@ -2,8 +2,23 @@
 import 'dotenv/config'
 import app from './app.js'
 import connectDB from './db/index.js'
+import cookieParser from 'cookie-parser'
+import express, { urlencoded } from "express"
 
-app.listen(process.env.PORT,()=>{
+const server = app.listen(process.env.PORT,()=>{
     console.log(`Server is running http://localhost:${process.env.PORT}/`)
 })
+
+server.on("error",(err)=>{
+    console.log("Server Error "+ err);
+    // throw err;
+    process.exit(1)
+})
+
+app.use(cookieParser())
+
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded())
+app.use(express.static("public"))
+
 connectDB() 
